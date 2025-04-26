a9622ba4e7ac258553926c3ceb3e31dcf73e95a4
corinneehrsam <96621296+corinneehrsam@users.noreply.github.com>
2025-04-26 13:34:04 +0200

    [ADD] pattern

--- CC/sketch.js
+++ CC/sketch.js
@@ -1,27 +1,36 @@
 function setup() {
-  createCanvas(600, 400);
+  createCanvas(500, 500);
   noLoop();
 }
 
 function draw() {
   background("lightgrey");
 
-  let x = 25;
-  let y = 25;
-  let r = random(5,50);
-  let cellSize = 50;
+  let x = 0;
+  let y = 0
+  let cellSize = 5;
+  
 
-  for(i = 0; i < 100; i++) {
-    fill(0);
-    noStroke();
-    r = random(5,50);
-    ellipse(x, y, r);
+  for(i = 0; i < 300000; i++) {
+    let n = random(0,1);    
+  
+    if(n < 0.5) {
+      stroke("purple");
+      strokeWeight(2);
+      line(x, y, x + cellSize, y + cellSize);
+    } else {
+      stroke("orange");
+      strokeWeight(4);
+      line(x + cellSize, y, x, y + cellSize);
+    } 
+
     x = x + cellSize;
 
-    if(x > width) {
-      x = 25;
+    if(x > height) {
+      x = 0;
       y = y + cellSize;
     }
+    
   }
 }
 
