3d75a0119fe93446a8297e84bccfdec0a1628f1d
corinneehrsam <96621296+corinneehrsam@users.noreply.github.com>
2025-04-26 15:21:07 +0200

    [ADD] experiments

--- CC/sketch.js
+++ CC/sketch.js
@@ -1,37 +1,94 @@
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(400,400);
   noLoop();
+
+  angleMode(DEGREES);
+
+  rectMode(CENTER);
 }
+
+// function draw() {
+//   background("lightgrey");
+
+//   let x = 0;
+//   let y = 0;
+//   let cellSize = 50;
+  
+
+//   for(i = 0; i < 200; i++) {
+//     let n = random(0,4);
+  
+//     fill("black");
+
+//     noStroke();
+
+//     if(n > 0) {
+//       fill("yellow");
+//       triangle(x, y, x + cellSize*0.5, y + cellSize*0.5, x, y + cellSize);
+//     } 
+//     if(n > 1) {
+//       fill("purple");
+//       triangle(x, y, x + cellSize, y, x + cellSize*0.5, y + cellSize*0.5);
+//     } 
+//     if(n > 2) {
+//       fill("blue");
+//       triangle(x + cellSize, y, x + cellSize, y + cellSize, x + cellSize*0.5, y + cellSize*0.5);
+//     }
+//     if(n > 3) {
+//       fill("orange");
+//       triangle(x + cellSize, y + cellSize, x + cellSize*0.5, y + cellSize*0.5, x, y + cellSize);
+//     }
+
+//     x = x + cellSize;
+
+//     if(x > height) {
+//       x = 0;
+//       y = y + cellSize;
+//     }
+    
+//   }
+// }
+
+// ------------------------------------------------------- 2ND
+
+// function draw() {
+//   background("lightgrey");
+
+//   let x = 0;
+//   let y = 0;
+//   let cellSize = 50;
+  
+//   noFill();
+
+//   for(i = 0; i < 200; i++) {
+//     let n = random(5,25);
+
+//     for(j = 0; j < 5; j++) {
+//       strokeWeight(round(random(1,3)));
+//       stroke(round(random(100,255)), 150, round(random(100,255)));
+//       ellipse(x + cellSize*0.5, y + cellSize*0.5, n * j, n * j);
+//     }
+
+//     x = x + cellSize;
+
+//     if(x > height) {
+//       x = 0;
+//       y = y + cellSize;
+//     }
+    
+//   }
+// }
 
 function draw() {
   background("lightgrey");
 
-  let x = 0;
-  let y = 0
-  let cellSize = 5;
-  
+  push();
+  translate(width/2, height/2);
+  rotate(20);
+  rect(0, 0, 200, 50);
+  pop();
 
-  for(i = 0; i < 300000; i++) {
-    let n = random(0,1);    
-  
-    if(n < 0.5) {
-      stroke("purple");
-      strokeWeight(2);
-      line(x, y, x + cellSize, y + cellSize);
-    } else {
-      stroke("orange");
-      strokeWeight(4);
-      line(x + cellSize, y, x, y + cellSize);
-    } 
-
-    x = x + cellSize;
-
-    if(x > height) {
-      x = 0;
-      y = y + cellSize;
-    }
-    
-  }
+  rect(50, 50, 200, 50);
 }
 
 function mousePressed() {
