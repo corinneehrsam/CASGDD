a5ca33a3bb75bc7db5090cbcaed52138308b3277
corinneehrsam <96621296+corinneehrsam@users.noreply.github.com>
2025-05-02 15:54:02 +0200

    [ADD] sinus wave with dots filled

--- CC/sketch.js
+++ CC/sketch.js
@@ -1,46 +1,47 @@
 function setup() {
-  createCanvas(800,500);
-
+  createCanvas(800, 500);
   //noLoop();
-  // pixelDensity(3); 
+  //pixelDensity(3); 
 }
 
 let b = 0;
-let A = 20;
+let A = 10;
 let k = 0.05;
 
 let b2 = 0;
-let A2 = 12;
+let A2 = 15;
 let k2 = 0.1;
 
 function draw() {
   background("black");
-  
-  push();
-  translate(0, height/2);
-  beginShape();
-  vertex(0, height/2);
-  for(let x=0; x<=width; x+=10) {
-    let y1 = wave(x, A, k, b);
-    let y2 = wave(x, A2, k2, b2);
-    let y3 = wave(x, 2, 0.5, frameCount * 0.2);
-    let y = y1 + y2 + y3;
-    vertex(x, y);
-  }
-  vertex(width, height/2);
-  endShape();
-  pop();
 
-  b = b + 0.08;
-  b2 = b2 + 0.05;
+  let numRows = 50;      
+  let spacing = 10; 
+
+  for (let row = 0; row < numRows; row++) {
+    let yOffset = row * spacing + 50;  
+    for (let x = 0; x <= width; x += 10) {
+      let y1 = wave(x, A, k, b);
+      let y2 = wave(x, A2, k2, b2);
+      let y = 50 + y1 + y2 + yOffset;
+
+      let alpha = map(y, 0, width, 255, 0);
+      fill(255, alpha);
+      noStroke();
+      ellipse(x, y, 5);
+    }
+  }
+
+  b += 0.08;
+  b2 += 0.05;
 }
 
-function wave(x,A,k,b) {
+function wave(x, A, k, b) {
   return A * sin(k * x + b);
 }
 
 function keyTyped() {
-  if(key === "s") {
+  if (key === "s") {
     saveCanvas('myCanvas', 'png');
   }
 }
