738adb12ece19891869295549cb79cfdf5928077
corinneehrsam <96621296+corinneehrsam@users.noreply.github.com>
2025-05-02 14:37:37 +0200

    [ADD] sinus experiment

--- CC/sketch.js
+++ CC/sketch.js
@@ -4,21 +4,29 @@ function setup() {
 
   //noLoop();
   rectMode(CENTER);
-  // pixelDensity(3); // Auflösung
+  // pixelDensity(3); 
 }
 
-let r = 100;
 let angle = 0;
+let r = 3;
+let spacing = 20;
+
 
 function draw() {
-  background("lightgrey");
+  background("black");
+  fill(255);
 
-  r = map(sin(angle), -1, 1, 100, 300);
+  for (let i = 0; i < height / spacing; i++) {
+    let yOffset = i * spacing;
+    let phaseShift = i * 80; 
 
-  stroke(0);
-  rect(width/2, height/2, r, r);
+    for (let x = 0; x < width; x += 10) {
+      let y = yOffset + 100 * sin(x + angle + phaseShift);
+      ellipse(x, y, r);
+    }
+  }
 
-  angle = angle + 5;
+  angle = angle + 1;
 }
 
 function keyTyped() {
