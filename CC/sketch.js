3ad8ad57ea3942e1fa218d41f738e5092b7ba5a8
corinneehrsam <96621296+corinneehrsam@users.noreply.github.com>
2025-05-02 15:33:14 +0200

    [ADD] wave b/w

--- CC/sketch.js
+++ CC/sketch.js
@@ -1,32 +1,42 @@
 function setup() {
-  createCanvas(500,500);
-  angleMode(DEGREES);
+  createCanvas(800,500);
 
   //noLoop();
-  rectMode(CENTER);
   // pixelDensity(3); 
 }
 
-let angle = 0;
-let r = 3;
-let spacing = 20;
+let b = 0;
+let A = 20;
+let k = 0.05;
 
+let b2 = 0;
+let A2 = 12;
+let k2 = 0.1;
 
 function draw() {
   background("black");
-  fill(255);
-
-  for (let i = 0; i < height / spacing; i++) {
-    let yOffset = i * spacing;
-    let phaseShift = i * 80; 
-
-    for (let x = 0; x < width; x += 10) {
-      let y = yOffset + 100 * sin(x + angle + phaseShift);
-      ellipse(x, y, r);
-    }
+  
+  push();
+  translate(0, height/2);
+  beginShape();
+  vertex(0, height/2);
+  for(let x=0; x<=width; x+=10) {
+    let y1 = wave(x, A, k, b);
+    let y2 = wave(x, A2, k2, b2);
+    let y3 = wave(x, 2, 0.5, frameCount * 0.2);
+    let y = y1 + y2 + y3;
+    vertex(x, y);
   }
+  vertex(width, height/2);
+  endShape();
+  pop();
 
-  angle = angle + 1;
+  b = b + 0.08;
+  b2 = b2 + 0.05;
+}
+
+function wave(x,A,k,b) {
+  return A * sin(k * x + b);
 }
 
 function keyTyped() {
