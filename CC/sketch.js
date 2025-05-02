cb213219b1af4976995fb5cb0e8344fd9d40a5e6
corinneehrsam <96621296+corinneehrsam@users.noreply.github.com>
2025-05-02 10:03:49 +0200

    [ADD] map function

--- CC/sketch.js
+++ CC/sketch.js
@@ -1,10 +1,12 @@
 function setup() {
-  createCanvas(400,400);
-  noLoop();
+  createCanvas(500,500);
+  //noLoop();
 
   angleMode(DEGREES);
 
   rectMode(CENTER);
+
+  // pixelDensity(3); // Auflösung
 }
 
 // function draw() {
@@ -51,46 +53,38 @@ function setup() {
 
 // ------------------------------------------------------- 2ND
 
-// function draw() {
-//   background("lightgrey");
-
-//   let x = 0;
-//   let y = 0;
-//   let cellSize = 50;
-  
-//   noFill();
-
-//   for(i = 0; i < 200; i++) {
-//     let n = random(5,25);
-
-//     for(j = 0; j < 5; j++) {
-//       strokeWeight(round(random(1,3)));
-//       stroke(round(random(100,255)), 150, round(random(100,255)));
-//       ellipse(x + cellSize*0.5, y + cellSize*0.5, n * j, n * j);
-//     }
-
-//     x = x + cellSize;
-
-//     if(x > height) {
-//       x = 0;
-//       y = y + cellSize;
-//     }
-    
-//   }
-// }
+let x = 50;
+let y = 50;
 
 function draw() {
   background("lightgrey");
 
-  push();
-  translate(width/2, height/2);
-  rotate(20);
-  rect(0, 0, 200, 50);
-  pop();
+  let n = 5;
 
-  rect(50, 50, 200, 50);
+  for(i=0; i<=n; i++) {
+    x = map(i, 0, n, 0, width);
+    ellipse(x,y,50,50);
+  }
 }
 
-function mousePressed() {
-  saveCanvas('myCanvas', 'png');
+// function draw() {
+//   background("lightgrey");
+
+//   push();
+//   translate(width/2, height/2);
+//   rotate(20);
+//   rect(0, 0, 200, 50);
+//   pop();
+
+//   rect(50, 50, 200, 50);
+// }
+
+// function mousePressed() {
+//   saveCanvas('myCanvas', 'png');
+// }
+
+function keyTyped() {
+  if(key === "s") {
+    saveCanvas('myCanvas', 'png');
+  }
 }
